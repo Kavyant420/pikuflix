58b01950b0038e9ca0881e5d69605b2612b19152
pikuflix <kavyantverma62@gmail.com>
2025-08-21 01:27:12 +0530

    Update series.js

--- series.js
+++ series.js
@@ -1,18 +1,17 @@
-movieDatabase.push(
-  {
-    id: 124364,
-    title: "From",
-    overview: "Unravel the mystery of a nightmarish town in middle America that traps all those who enter. As the unwilling residents search for a way out, they must also survive the threats of the surrounding forest.",
-    first_air_date: "2022-02-20",
-    vote_average: 8.3,
-    poster_path: "https://image.tmdb.org/t/p/original/uV65yaFkw6B4KHXAsBYui0KvJq.jpg",
-    media_type: "tv",
-    number_of_seasons: 2,
-    number_of_episodes: 20,
-    genres: ["Mystery", "Sci-Fi & Fantasy", "Drama", "Horror"],
-    views: "50M",
-    quality: "HD",
-    tmdb_rating: 8.3,
-    popularity: "High"
-  }
-);
+const apiKey = "8d18cc3ec326ca4282a7ab5a651c7f7b";
+
+fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`)
+  .then(res => res.json())
+  .then(data => {
+    data.results.forEach(show => {
+      movieDatabase.push({
+        id: show.id,
+        title: show.name,
+        overview: show.overview,
+        release_date: show.first_air_date,
+        vote_average: show.vote_average,
+        poster_path: "https://image.tmdb.org/t/p/original" + show.poster_path,
+        media_type: "tv"
+      });
+    });
+  });
